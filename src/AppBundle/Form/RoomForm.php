e19b6c0d08e996fdd52eeec4dee566bdcf374cee
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-01-26 16:11:42 +0100

    Added missing use of AbstractType when adding a room

--- src/AppBundle/Form/RoomForm.php
+++ src/AppBundle/Form/RoomForm.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\AbstractType;
 
 class RoomForm extends AbstractType
 {
